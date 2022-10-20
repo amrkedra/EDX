6fdda3d712b6e68f5d8c2c21ff0113c91b83a2c4
amrkedra <amrkedra.1993@gmail.com>
2022-10-20 11:36:41 +0200

    branch inside a loop, how many big cities

---
+++ branch inside a loop, how many big cities.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main (void){
+int i , num_of_cities , population , population_count=0 ;
+printf("please enter the number of cities :");
+scanf("%d" , &num_of_cities);
+
+for(i=1; i<num_of_cities+1; i++){
+
+printf("please enter the population number of city no %d :" , i);
+scanf("%d" , &population);
+
+if (population > 10000 ){
+
+    population_count = population_count +1;
+}
+
+}
+ 
+ printf("cities with Population more than 10000 are : %d", population_count);
+
+
+
+
+return 0 ;
+
+}
