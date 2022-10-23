74ce74e9282a9d902664a40b86b5295b1cb6faa7
amrkedra <amrkedra.1993@gmail.com>
2022-10-23 18:54:11 +0200

    Create guess my number.c

---
+++ guess my number.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main (void){
+int des_num , guessed_num , i;
+printf("please enter the desired number :");
+scanf("%d" , &des_num);
+
+
+while(guessed_num != des_num){
+    
+    printf("please enter the guessed num");
+    scanf("%d" , &guessed_num);
+    
+    if (guessed_num > des_num)
+    {
+        printf("the guessed number is more than the needed number please enter another number :");
+        scanf("%d" , &guessed_num);
+    }
+    else if (guessed_num < des_num){
+        printf("the guessed numer is less than the needed number please enter another number:  ");
+        scanf("%d" , &guessed_num);
+    }
+
+    i++;
+}
+
+
+    printf("you have reached the needed number ");
+
+
+
+return 0 ;
+
+}
