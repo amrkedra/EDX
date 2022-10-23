e7ef3717dd773533df909c13f64eb636ddd25954
amrkedra <amrkedra.1993@gmail.com>
2022-10-23 10:59:10 +0200

    Create controlling an epidemic .c
    In order to be able to better fight various epidemics in a region, the department of medicine of a major university has launched a large study. Researchers are interested in how fast an epidemic spreads, and therefore the speed at which health measures must be put in place. Your program should first read an integer representing the total population of the area. Knowing that a person was infected on day 1 and that each infected person contaminates two new people every day, you must calculate the day at which the entire population of the area will be infected.

---
+++ controlling an epidemic .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main (void){
+int population , day=1 , infected=1 ;
+printf("please enter the population : ");
+scanf("%d" , &population);
+
+while(infected < population){
+
+day = day + 1;
+
+infected=infected*3;
+
+}
+
+printf("the entire city will take %d days to be infected" , day);
+
+return 0 ;
+
+}
